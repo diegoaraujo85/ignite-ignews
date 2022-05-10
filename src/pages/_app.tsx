c36a400f9bb2d0f68879755422d5dbb062b22b3e
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-05-10 14:29:46 -0300

    original myapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,14 @@
-import { AppProps as NextAppProps } from "next/app";
+import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { Provider as NextAuthProvider } from "next-auth/client";
 
-// modified version - allows for custom pageProps type, falling back to 'any'
-type AppProps<P = any> = {
-  pageProps: P;
-} & Omit<NextAppProps<P>, "pageProps">;
-
 import "../styles/global.scss";
 
-function MyApp({
-  Component,
-  pageProps,
-  err,
-}: AppProps & { err: any }): JSX.Element {
-  // Workaround for https://github.com/vercel/next.js/issues/8592
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <>
-        <Header />
-        <Component {...pageProps} err={err} />
-      </>
+      <Header />
+      <Component {...pageProps} />
     </NextAuthProvider>
   );
 }

b1ffb8f9a7b57dc4ba1c492d084e5a6437520b26
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-05-10 14:17:56 -0300

    myapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
-import { AppProps } from "next/app";
+import { AppProps as NextAppProps } from "next/app";
 import { Header } from "../components/Header";
 import { Provider as NextAuthProvider } from "next-auth/client";
+
+// modified version - allows for custom pageProps type, falling back to 'any'
+type AppProps<P = any> = {
+  pageProps: P;
+} & Omit<NextAppProps<P>, "pageProps">;
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
+      <>
+        <Header />
+        <Component {...pageProps} />
+      </>
     </NextAuthProvider>
   );
 }
